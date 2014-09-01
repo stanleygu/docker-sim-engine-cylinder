fec17b0c9402f40039f646822c5757ff027f5d7c
Stanley Gu <stanleygu@gmail.com>
2014-09-01 01:14:28 +0000

    Added method chaining and override model load
    to prevent unnecessary loading

--- cylinder.py
+++ cylinder.py
@@ -1,5 +1,6 @@
 import roadrunner
 import os
+import md5
 
 from celery import Celery
 
@@ -8,6 +9,8 @@ app = Celery(
     backend=os.environ['BROKER_URL'],
     broker=os.environ['BROKER_URL'])
 rr = roadrunner.RoadRunner()
+
+currentModel = ''
 
 
 @app.task
@@ -18,10 +21,28 @@ def add(x, y):
 @app.task
 def rrRun(method, *params):
     if (method is not None):
-        if (isinstance(method, basestring)):
+        if (method == 'load'):
+            # Override load method
+            newModel = md5.new(*params).digest()
+            global currentModel
+            if (newModel == currentModel):
+                # Model already loaded
+                pass
+            else:
+                currentModel = newModel
+                rr.load(*params)
+        elif (isinstance(method, basestring)):
             return getattr(rr, method)(*params)
         else:
             fun = getattr(rr, method.pop(0))
             for m in method:
                 fun = getattr(fun, m)
             return fun(*params)
+
+
+@app.task
+def rrChain(commands):
+    results = []
+    for command in commands:
+        results.append(rrRun(*command))
+    return results
