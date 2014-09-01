fec2e420479b6e03644a0ce0d290af1ce6ff8fcf
Stanley Gu <stanleygu@gmail.com>
2014-09-01 03:10:03 +0000

    v0.0.2 with python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import json
+
+with open('package.json', 'r') as f:
+    version = json.load(f)['version']
+
+setup(name='simworker',
+      version=version,
+      description='Distributed simulation',
+      author='Stanley Gu',
+      author_email='stanleygu@gmail.com',
+      url='https://github.com/stanleygu/docker-sim-engine-cylinder.git',
+      packages=['']
+      )
